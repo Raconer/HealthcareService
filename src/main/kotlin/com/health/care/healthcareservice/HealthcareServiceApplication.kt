2249bc9c9e02fa0f487326117eb2e4df92099bb1
kimdongho <cha11enge@hanmail.net>
2025-08-21 22:48:40 +0900

    first commit

---
+++ src/main/kotlin/com/health/care/healthcareservice/HealthcareServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.health.care.healthcareservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthcareServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealthcareServiceApplication>(*args)
+}
